9e1daedcf601b3bd4ab0ff92736ac29e1fdfacb8
mua42 <248243032+mua42@users.noreply.github.com>
2026-05-12 07:45:51 +0100

    added the function to monitor the changes, updating the device files, and checking if there has been any changes

--- cron-watcher.py
+++ cron-watcher.py
@@ -1,5 +1,60 @@
-# Pulls the repo
 # Monitor the changes in the cronjob files and current file
 # If there is a change
+    # Pulls the repo
     # Update the local cronjobs
     # Push it up to GitHub
+
+import os
+import subprocess
+from crontab import CronTab
+from dotenv import load_dotenv
+from pathlib import Path
+
+# load the .env file
+load_dotenv()
+# get the device name and file path
+DEVICE_NAME = os.getenv("DEVICE_NAME")
+DEVICE_FILE_PATH = os.getenv("DEVICE_PATH")
+# gets the crontabs of the current user only
+cron = CronTab(user=True)
+
+
+
+def get_original_cronjobs():
+    original_cronjobs = subprocess.run(
+        ["crontab", "-l"],
+        capture_output=True,
+        text=True
+    )
+
+    if original_cronjobs.returncode == 0:
+        return original_cronjobs
+    else:
+        exit(0)
+
+
+# monitors the string output of the cronjobs compared to the device file
+# returns True if there is changes, returns False if no changes
+def monitor_cron_changes(original_cronjobs):
+    device_cronjobs = Path(DEVICE_FILE_PATH).read_text()
+
+    if device_cronjobs != original_cronjobs.stdout:
+        return True
+    else:
+        return False
+
+
+# adds the contents of the original cronjobs to the device file
+def update_device_file(original_cronjobs):
+    with open(DEVICE_FILE_PATH, "w") as f:
+        f.write(original_cronjobs.stdout)
+
+
+# if there are changes in the cronjob then execute the changes
+original_cronjobs = get_original_cronjobs()
+if monitor_cron_changes(original_cronjobs):
+    # print("different")
+    update_device_file(original_cronjobs)
+else:
+    # print("same")
+    pass
