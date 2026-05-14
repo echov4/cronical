766bb50b6c76a289a412e9702f5fac252e3c64d5
mua42 <248243032+mua42@users.noreply.github.com>
2026-05-14 03:26:28 +0100

    auto: update crons for device [new]

--- generate_ics.py
+++ generate_ics.py
@@ -1,6 +1,8 @@
 # get all the the files in crons/ and their contents
-# parse through each job
-# check for comments
+
+# store as a dict
+# remove all the comments and strip and trim the jobs
+
 # get the time for each job and parse it to date time in ICS
 # generate the ICS file
 # save it to the public folder
@@ -8,25 +10,29 @@
 
 from pathlib import Path
 import os
+from crontab import CronTab
+
 
 PATH = Path(__file__).parent
 CRONS_DIRECTORY = "crons/"
 
+# list of  dicts
 ALL_CRONS_FILES = []
 
 def get_device_file_crons():
     # get all the files
     all_device_files= os.listdir(PATH/CRONS_DIRECTORY)
 
-    # check if one file exists
+    # check if atleast one file exists
     if len(all_device_files) == 0:
         print("Error: no device files, need to set it up")
         exit(1)
 
     # get all the contents of the files
     for file in all_device_files:
-        contents = (PATH/CRONS_DIRECTORY/file).read_text()
-        ALL_CRONS_FILES.append(contents.strip())
+        parse_crons(file)
+
+
         # print((PATH/CRONS_DIRECTORY/file).read_text())
 
     for a in ALL_CRONS_FILES:
@@ -39,8 +45,18 @@ def get_device_file_crons():
 
 
 
-def parse_crons():
-    pass
+def parse_crons(file):
+    print(file)
+    # contents = (PATH/CRONS_DIRECTORY/file).read_text()
+    # cron = CronTab(tab="""
+    # # backup job
+    # */5 * * * * /usr/bin/python backup.py # run backup
+    # """)
+
+    # for job in cron:
+    #     print(job.slices)      # cron timing
+    #     print(job.command)     # command
+    #     print(job.comment)     # comment
 
 
 
@@ -50,6 +66,4 @@ def generate_ics_file():
 
 
 
-
-
-get_device_file_crons()
+# get_device_file_crons()
