b8de2078e72420e71665b61a30d052ea5d8ed163
mua42 <248243032+mua42@users.noreply.github.com>
2026-05-18 09:01:34 +0100

    updated

--- setup.py
+++ setup.py
@@ -1,23 +1,20 @@
 import subprocess
-import os
 from crontab import CronTab
 from pathlib import Path
 import shutil
 
-from datetime import datetime
 import logging
 from logging.handlers import RotatingFileHandler
-
-# gets the cron tabs of the current user only
-cron = CronTab(user=True)
 
 # global
 PATH = Path(__file__).parent
 WATCHER_FILE = "cron-watcher.py"
 CRONS_DIRECTORY= "crons"
 
-# need to make the logs directory if it does not exist for the logging to work
+# need to make the logs, crons, and public directories (will need logs/ for logging to work)
 (PATH / "logs").mkdir(exist_ok=True)
+(PATH / "crons").mkdir(exist_ok=True)
+(PATH / "public").mkdir(exist_ok=True)
 
 # setup logging with 1MB limit and keep 3 backups
 rotating_handler = RotatingFileHandler(
@@ -50,7 +47,7 @@ def checkup_and_set_environment():
         logger.info("Git is installed")
 
     # make a .venv folder
-    if ".venv" not in os.listdir(PATH):
+    if not (PATH / ".venv").exists():
         logger.error("ERROR: .venv does not exist, run `uv sync`")
         exit(1)
     else:
@@ -62,23 +59,6 @@ def checkup_and_set_environment():
     # return the path to the python runtime for the cronjob runtime
     return python_venv_runtime_path
 
-
-# check and create the crons folder
-def create_cron_folder():
-    if CRONS_DIRECTORY in os.listdir(PATH):
-        logger.info(f"{CRONS_DIRECTORY}/ directory already exists")
-    else:
-        os.mkdir(PATH/CRONS_DIRECTORY)
-        logger.info(f"Created the {CRONS_DIRECTORY}/ directory")
-
-
-# check if public folder if not there, create it
-def create_public_folder():
-    if "public" in os.listdir(PATH):
-        logger.info("public/ directory already exists")
-    else:
-        os.mkdir(PATH / "public")
-        logger.info("Created public/ directory")
 
 # Get the device name and check if it already exists as a file in /crons, if not then create it
 # returns the device name in string, and device file path
@@ -109,9 +89,12 @@ def create_device_file():
 
 # add the watcher script to the original cron tab for every minute update
 def add_watcher_to_crontab(python_venv_runtime_path):
+    # gets the cron tabs of the current user only
+    cron = CronTab(user=True)
+
     watcher_command = f"{python_venv_runtime_path} {PATH}/{WATCHER_FILE}"
 
-    # check if the watcher script is already added into the cron tab
+    # check if the watcher script is already added into the cron tab then skip
     watcher_added_status = False
     for job in cron:
         if watcher_command in job.command:
@@ -119,7 +102,7 @@ def add_watcher_to_crontab(python_venv_runtime_path):
             break
 
     # if the watcher script command is not in the original cron tab, then add the watcher script command
-    if watcher_added_status == False:
+    if not watcher_added_status:
         job = cron.new(command=watcher_command)
         job.minute.every(1)
         job.set_comment("cronical-watcher")
@@ -147,56 +130,27 @@ def add_original_cronjobs_to_device_file(device_file):
         logger.error(f"ERROR: cannot read original cron jobs to add to the device file {device_file}")
 
 
-# setup the env file if it does not exist
+# setup the env file if it does not exist, and overwrites it if it does
 def setup_env_file(device_file, device_name):
     github_user = input("Enter in your github username (case sensitive)\t")
     github_repo_name = input("Enter in the repo of the github (case sensitive)\t")
     github_pat = input("Enter in your github PAT code\t")
 
+    # overwrites the .env with the new information
     with open(PATH / ".env", "w") as f:
-        f.writelines(f"DEVICE_NAME={device_name}\n")
-        f.writelines(f"DEVICE_PATH={device_file}\n")
-        f.writelines(f"GITHUB_PAT={github_pat}\n")
-        f.writelines(f"GITHUB_USER={github_user}\n")
-        f.writelines(f"GITHUB_REPO_NAME={github_repo_name}\n")
+        f.write(f"DEVICE_NAME={device_name}\n")
+        f.write(f"DEVICE_PATH={device_file}\n")
+        f.write(f"GITHUB_PAT={github_pat}\n")
+        f.write(f"GITHUB_USER={github_user}\n")
+        f.write(f"GITHUB_REPO_NAME={github_repo_name}\n")
     logger.info("if .env file already existed, it has been overwritten with the new values")
     logger.info(f".env file has been created at {PATH / '.env'}")
-
-# sets the .gitignore file and adds the .env file in there
-def setup_gitignore():
-    # check if gitignore exists
-    gitignore = PATH / ".gitignore"
-    if not gitignore.exists():
-        gitignore.open("w").close()
-        logger.info(f"Creating a .gitignore file at {gitignore}")
-    else:
-        logger.info(f".gitignore file already exists at {gitignore}")
-
-    # check if .env file is in the gitignore
-    if ".env" not in gitignore.read_text():
-        with open(gitignore, "a") as f:
-            f.write(".env\n")
-        logger.info(f".env added to .gitignore at {gitignore}")
-    else:
-        logger.info(f".env already in .gitignore at {gitignore}")
-
-    # check if .venv/ folder is in gitignore
-    if ".venv/" not in gitignore.read_text():
-        with open(gitignore, "a") as f:
-            f.write(".venv/\n")
-        logger.info(f".venv/ added in .gitignore at {gitignore}")
-    else:
-        logger.info(f".venv/ already in .gitignore at {gitignore}")
-
 
 
 # MAIN
 python_venv_runtime_path = checkup_and_set_environment()
-create_cron_folder()
-create_public_folder()
 device_file, device_name = create_device_file()
 add_original_cronjobs_to_device_file(device_file)
 add_watcher_to_crontab(python_venv_runtime_path)
 setup_env_file(device_file, device_name)
-setup_gitignore()
 logger.info("Setup completed")
